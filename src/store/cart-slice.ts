992ca655aee7b32cdbe209fb1379750cdb4fa60e
nenadmne <cosovicnenad14@gmail.com>
2023-11-13 19:49:27 +0100

    Connecting addToCart with product component / making type safe useDispatch custom hook

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -23,8 +23,28 @@ export const cartSlice = createSlice({
       state,
       action: PayloadAction<{ id: string; title: string; price: number }>
     ) {
-      const itemIndex = state.items.findIndex((item) => item.id === action.id);
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex >= 0) {
+        state.items[itemIndex].quantity++;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
     },
-    removeFromCart() {},
+    removeFromCart(state, action: PayloadAction<string>) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      );
+
+      if (state.items[itemIndex].quantity === 1) {
+        state.items.splice(itemIndex, 1);
+      } else {
+        state.items[itemIndex].quantity--;
+      }
+    },
   },
 });
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
